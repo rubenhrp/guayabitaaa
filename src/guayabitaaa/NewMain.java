dd47bb15c7d4f7ab2fe38dc4d7887f6e7fbcf60d
ruben <ruben@LAPTOP-64ST65JE>
2023-01-27 00:58:39 -0500

    guayabitaa 5.0

--- src/guayabitaaa/NewMain.java
+++ src/guayabitaaa/NewMain.java
@@ -7,6 +7,10 @@ public class NewMain {
     public static void main(String[] args) {
         
         System.out.println("Bienvenido al juego de la Guayabita");
+        System.out.println("Sus reglas son:");
+        System.out.println("Primer regla: si el Dado cae en la cara del uno , el judador que lanzo el dado tendra que poner una moneda en la mesa");
+        System.out.println("Segunda regla: Si el Dado caen entre 2 y 5 el jugador tendra que apostar 'ojo la apuesta no tiene que superar lo que halla en la mesa'");
+        System.out.println("Tercera regla:si el dado cae en 6 todos los jugadores tendran que poner sobre la mesa");
         
         Turnos t= new Turnos();
         
